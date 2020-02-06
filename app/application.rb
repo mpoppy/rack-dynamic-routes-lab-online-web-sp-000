ff67f6683d5753f34a9aec6a8601f22f5bc7f529
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:16:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,11 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path == "/items"
-      resp.write 
+      resp.write
+    else
+      resp.write "Route not found"
+      resp.status = 404
+
   end
 
 
