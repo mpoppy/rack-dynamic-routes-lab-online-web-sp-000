3ef87705a1fc8aef12cd8b8cec68cdd930261f7b
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:25:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     if req.path == "/items"
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-      resp.write item.price
+        resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
