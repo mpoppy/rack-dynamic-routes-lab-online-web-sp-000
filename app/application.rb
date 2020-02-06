39fc165fd2e5b62de09164dbf4c4675c1aea4b7b
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:25:37 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,8 @@ class Application
       if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
       else
+        resp.write "Item not found"
+        resp.status = 400
       end
     else
       resp.write "Route not found"
