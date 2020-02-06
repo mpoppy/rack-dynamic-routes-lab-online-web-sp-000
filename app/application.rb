63f3d38971c8bfdd82a36120e80c2fcaa3b762db
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:24:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,7 @@ class Application
 
     if req.path == "/items"
       item_name = req.path.split("/items/").last
+      if item = @@items.find{|i| i.name == item_name}
       resp.write item.price
     else
       resp.write "Route not found"
