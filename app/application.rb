b6850f42cc8a094b9324234ca81544121aa7cf23
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:27:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     if req.path == "/items"
       item_name = req.path.split("/items/").last
       if item = @@items.find{|i| i.name == item_name}
-        resp.write item.price
+        resp.write "#{item.price}"
       else
         resp.write "Item not found"
         resp.status = 400
