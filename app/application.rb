cfb0eed80b22d104aab2eec33f65839dad51b786
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:15:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,4 +4,11 @@ class Application
   #should only allow request /items/<ITEM NAME> or else 404 error
   #if correct path, it should return the path
   #if item doesnt exist than 400 and an error msg
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+  end
+
+
 end
