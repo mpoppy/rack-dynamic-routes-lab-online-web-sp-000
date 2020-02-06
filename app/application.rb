5b6c4173b44965234b75d039c880c64ce9814d8b
Marc Poppy <poppy.marc@gmail.com>
2020-02-06 05:13:38 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,3 +1,7 @@
 
 class Application
+
+  #should only allow request /items/<ITEM NAME> or else 404 error
+  #if correct path, it should return the path
+  #if item doesnt exist than 400 and an error msg
 end
